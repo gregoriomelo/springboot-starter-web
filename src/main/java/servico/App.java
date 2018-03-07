da5c737ef540c6f82570c53c7d282ac6a7b5f1e3
Gregório Melo <gregorio@gregoriomelo.com>
2018-03-07 08:34:17 -0300

    Removes Swagger stuff

---
+++ src/main/java/servico/App.java
@@ -0,0 +1,15 @@
+package servico;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class App extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        new App()
+                .configure(new SpringApplicationBuilder(App.class))
+                .run(args);
+    }
+}
--- src/main/java/servico/demo/SampleController.java
+++ src/main/java/servico/demo/SampleController.java
@@ -1,10 +1,5 @@
 package servico.demo;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.annotations.ApiModelProperty;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,15 +12,11 @@ import java.util.Collection;
 @RequestMapping("/messages")
 public class SampleController {
 
-    @ApiOperation("hello")
-    @ApiResponse(code = 200, message = "Success", response = Message.class)
 	@GetMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public Message home() {
 		return new Message("Hello, world!");
 	}
 
-    @ApiOperation("messages")
-    @ApiResponses(value = { @ApiResponse(code = 200, message = "Success", response = Messages.class) })
 	@GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Messages messages() {
 	    return new Messages(Arrays.asList(new Message("oi"),
@@ -52,7 +43,6 @@ public class SampleController {
             this.message = message;
         }
 
-        @ApiModelProperty(notes = "The message", required = true)
         public String getMessage() {
             return message;
         }
